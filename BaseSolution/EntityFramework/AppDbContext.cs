fa2fe0d46f663c8f2e65dec16ebe3b2f782c85f8
unknown <antoninguyot98@gmail.com>
2020-09-23 09:10:21 +0200

    Initial Commit

---
+++ BaseSolution/EntityFramework/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace EntityFramework
+{
+    public class AppDbContext : DbContext
+    {
+        internal static AppDbContext context;
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected AppDbContext()
+        {
+        }
+
+        public static void Register(AppDbContext dbContext)
+        {
+            if(dbContext == null)
+            {
+                throw new ArgumentNullException("No Context");
+            }
+
+            context = dbContext;
+        }
+    }
+}
